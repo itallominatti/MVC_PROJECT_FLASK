a1faddd6a4411598eb074d191a2456610e5b2f35
itallominatti <itallominatti@gmail.com>
2024-12-16 16:32:28 -0300

    Add PersonCreatorController with person creation logic and validation

--- src/controllers/person_creator_controller.py
+++ src/controllers/person_creator_controller.py
@@ -1,5 +1,52 @@
 from src.models.sqlite.interfaces.people_repository import PeopleRepositoryInterface
+import re
 
 class PersonCreatorController:
     def __init__(self, people_repository: PeopleRepositoryInterface) -> None:
         self.__people_repository = people_repository
+
+        def create(self, person_info: dict) -> dict:
+            first_name = person_info.get('first_name')
+            last_name = person_info.get('last_name')
+            age = person_info.get('age')
+            pet_id = person_info.get('pet_id')
+
+            self.__validate_first_and_last_name(first_name, last_name)
+            self.__insert_person_in_db(first_name, last_name, age, pet_id)
+            formatted_response = self.__format_response(person_info)
+            return formatted_response
+
+            
+        def __validate_first_and_last_name(self,first_name: str, last_name: str) -> None:
+            """_summary_
+
+            Args:
+                first_name (str): _description_
+                last_name (str): _description_
+
+            Raises:
+                Exception: _description_
+            """
+            non_valid_caracters = re.compile(r'[^a-zA-Z]')
+            if non_valid_caracters.search(first_name) or non_valid_caracters.search(last_name):
+                raise Exception('Invalid first or last name')
+            
+        def __insert_person_in_db(self, first_name: str, last_name: str, age: int, pet_id: int) -> None:
+            """_summary_
+
+            Args:
+                first_name (str): _description_
+                last_name (str): _description_
+                age (int): _description_
+                pet_id (int): _description_
+            """
+            self.__people_repository.insert_person(first_name, last_name, age, pet_id)
+
+        def __format_response(self, person_info: dict) -> dict:
+            return {
+                "data": {
+                    "type": "Person",
+                    "count": 1,
+                    "attributes": person_info
+                }
+            }
