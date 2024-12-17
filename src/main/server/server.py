08ca2906517222bfeacced5858785aba0ddd6185
itallominatti <itallominatti@gmail.com>
2024-12-17 08:28:45 -0300

    Add pets_routes blueprint and implement list_pets endpoint

--- src/main/server/server.py
+++ src/main/server/server.py
@@ -2,9 +2,13 @@ from flask import Flask
 from flask_cors import CORS
 from src.models.sqlite.settings.connection import DBConnectionHandler
 
+from src.main.routes.pets_routes import pet_route_bp
+
 db_connection_handler = DBConnectionHandler()
 
 app = Flask(__name__)
 CORS(app)
+
+app.register_blueprint(pet_route_bp)
 
 
