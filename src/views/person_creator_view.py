2ab270ac15acec11cf9916cb484c1fa11e5d7b0c
itallominatti <itallominatti@gmail.com>
2024-12-17 07:45:29 -0300

    Refactor ViewInterface to use http_request parameter and add PersonCreatorView for handling person creation

---
+++ src/views/person_creator_view.py
@@ -0,0 +1,14 @@
+from .http_types.http_request import HttpRequest
+from .http_types.http_response import HttpResponse
+from .interfaces.view_interface import ViewInterface
+from src.controllers.interfaces.person_creator_controller import PersonCreatorInterface
+
+class PersonCreatorView(ViewInterface):
+    def __init__(self, controller: PersonCreatorInterface) -> None:
+        self.__controller = controller
+
+    def handle(self, http_request: HttpRequest) -> HttpResponse:
+        person_info = http_request.body
+        body_response = self.__controller.create(person_info)
+
+        return HttpResponse(status_code=201, body=body_response)
